1aaa25ce50825985bd523646bc84aaccb1e5f43f
mgcllee <mgcllee@gmail.com>
2025-02-09 15:28:09 +0900

    [Common][new class] ChatMsg

---
+++ ChatRoom.Common/ChatMsg.cs
@@ -0,0 +1,12 @@
+﻿using Orleans;
+
+namespace ChatRoom;
+
+// TODO: GenerateSerializer, Immutable 연구
+[GenerateSerializer, Immutable]
+public record class ChatMsg(
+    string? Author,
+    string Text)
+{
+    // TODO: record class keyword 연구
+}
