26401448bbf7f4823c765810840db861d965aec7
mgcllee <mgcllee@gmail.com>
2025-04-04 18:34:06 +0900

    [Server][Blazor] Init Blazor Client

---
+++ TrickFarmWebClient/TrickFarmWebClient/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+public class ChatHub : Hub
+{
+    public string SendMessageToTcpServer(string message)
+    {
+        return $"서버로부터 응답: {message.ToUpper()}";
+    }
+
+    public async Task BroadcastMessage(string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", message);
+    }
+}
