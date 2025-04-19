696c8e4a0ce77a2b1e91a2844bf066367e87e30d
mgcllee <mgcllee@gmail.com>
2025-04-19 19:28:25 +0900

    [Server][update] Add CMake (Linux) project

---
+++ TrickFarmServer/TrickFarmNetworkServer/TrickFarmNetworkServer.cpp
@@ -0,0 +1,12 @@
+﻿// TrickFarmNetworkServer.cpp : Defines the entry point for the application.
+//
+
+#include "TrickFarmNetworkServer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
