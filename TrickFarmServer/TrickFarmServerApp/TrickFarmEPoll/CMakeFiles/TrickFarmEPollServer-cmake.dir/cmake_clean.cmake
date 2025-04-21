570f15198015d8f055f49818490b9c0d26398ffa
mgcllee <mgcllee@gmail.com>
2025-04-21 19:09:05 +0900

    [IDE][update] VSCode, Dev Container

---
+++ TrickFarmServer/TrickFarmServerApp/TrickFarmEPoll/CMakeFiles/TrickFarmEPollServer-cmake.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TrickFarmEPollServer-cmake.dir/TrickFarmEPollServer.cpp.o"
+  "CMakeFiles/TrickFarmEPollServer-cmake.dir/TrickFarmEPollServer.cpp.o.d"
+  "TrickFarmEPollServer-cmake"
+  "TrickFarmEPollServer-cmake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TrickFarmEPollServer-cmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ TrickFarmServer/TrickFarmServerApp/TrickFarmEPoll/TrickFarmEPollServer.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+#include <unistd.h>
+#include <sys/epoll.h>
+
+int main() {
+    std::cout << "Hello from std::cout!" << std::endl;
+    printf("만나서 반가워요\n");
+    return 0;
+}
