96b394db82bcb6dd5feed1ba3fe4e002c3932da8
mgcllee <mgcllee@gmail.com>
2025-04-23 16:29:15 +0900

    [EPollServer][update] make Common struct

--- TrickFarmServer/TrickFarmServerApp/TrickFarmEPoll/TrickFarmEPollServer.cpp
+++ TrickFarmServer/TrickFarmServerApp/TrickFarmEPoll/TrickFarmEPollServer.cpp
@@ -5,22 +5,30 @@
 #include <unistd.h>
 #include <cstring>
 #include <iostream>
+#include <sys/epoll.h>
 
-const char* shm_name = "/ipc_shm";
-const char* sem_c2csharp = "/sem_c2csharp";
-const char* sem_csharp2c = "/sem_csharp2c";
-const size_t shm_size = 1024;
+#include "Common/Common.h"
+
+const char* sem_cpp2csharp = "/sem_c2csharp";
+const char* sem_csharp2cpp = "/sem_csharp2c";
 
 int main() {
-    int shm_fd = shm_open(shm_name, O_CREAT | O_RDWR, 0666);
-    ftruncate(shm_fd, shm_size);
-    void* ptr = mmap(0, shm_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    int shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
+    ftruncate(shm_fd, SHM_SIZE);
+    void* ptr = mmap(0, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
 
-    sem_t* sem_csharp = sem_open(sem_c2csharp, O_CREAT, 0666, 0);
-    sem_t* sem_cpp = sem_open(sem_csharp2c, O_CREAT, 0666, 0);
+    sem_t* sem_csharp = sem_open(sem_cpp2csharp, O_CREAT, 0666, 0);
+    sem_t* sem_cpp = sem_open(sem_csharp2cpp, O_CREAT, 0666, 0);
+
+    
 
     char* data = (char*)ptr;
     for (int i = 0; i < 10; ++i) {
+
+        // Using Async EPoll
+
+        // Recv TCP data packet
+
         while (data[0] != 0) usleep(100);
         data[0] = 1;  // writing
 
@@ -29,8 +37,8 @@ int main() {
         memcpy(data + 1, &len, 4);
         memcpy(data + 5, msg.c_str(), len);
 
-        sem_post(sem_csharp);  // notify C#
-        sem_wait(sem_cpp);     // wait for response
+        sem_post(sem_csharp);  // [notify C#]
+        sem_wait(sem_cpp);     // [wait for response]
 
         uint32_t res_len;
         memcpy(&res_len, data + 1, 4);
@@ -40,11 +48,11 @@ int main() {
         data[0] = 0;  // reset flag
     }
 
-    munmap(ptr, shm_size);
+    munmap(ptr, SHM_SIZE);
     close(shm_fd);
-    shm_unlink(shm_name);
+    shm_unlink(SHM_NAME);
     sem_close(sem_csharp);
     sem_close(sem_cpp);
-    sem_unlink(sem_c2csharp);
-    sem_unlink(sem_csharp2c);
+    sem_unlink(sem_cpp2csharp);
+    sem_unlink(sem_csharp2cpp);
 }
--- TrickFarmServer/TrickFarmServerApp/TrickFarmOrleansServer/TcpChatServer.cs
+++ TrickFarmServer/TrickFarmServerApp/TrickFarmOrleansServer/TcpChatServer.cs
@@ -22,45 +22,49 @@ class TcpChatServer
         this.redis_connector = redis_connector;
         _listener.Start();
 
-        Console.WriteLine("[Start] server listening socket");
+        Console.WriteLine("[Server][Start] listening client socket");
 
         while (true)
         {
             var client_socket = await _listener.AcceptTcpClientAsync();
-            _ = HandleClientAsync(client_socket);
+            if(client_socket is null || false == client_socket.Connected) {
+                continue;
+            }
+
+            byte[] bytes = new byte[1024];
+            int name_len = await client_socket.GetStream().ReadAsync(bytes, 0, bytes.Length);
+            string user_name = Encoding.UTF8.GetString(bytes, 0, name_len);
+
+            var user_guid = client_connector.add_client_connector(client_socket);
+            if (name_len > 0 && user_name is not "leave" 
+                && user_name.Length <= 10 
+                && user_name.Contains(" ")
+                && redis_connector.write_user_info(user_guid, user_name))
+            {
+                var client_grain = _grainFactory.GetGrain<IChatClientGrain>(user_guid);
+                Console.WriteLine($"Redis에 기록 성공! 어서오세요 {user_name}님");
+                ClientRequestWorkerAsync(client_socket);
+            }
+            else
+            {
+                redis_connector.delete_user_info(user_guid);
+                Console.WriteLine("Redis에 기록 실패...");
+
+                client_connector.discoonect_client(user_guid);
+                Console.WriteLine("클라이언트 연결 해제됨");
+            }
         }
     }
 
-    private async Task HandleClientAsync(TcpClient client_socket)
+    private async Task ClientRequestWorkerAsync(TcpClient client_socket, IChatClientGrain client_grain)
     {
-        var user_guid = client_connector.add_client_connector(client_socket);
-        var client = client_connector.get_client_connector(user_guid);
-        
-        if(client == null)
+        while(true) 
         {
-            client_connector.discoonect_client(user_guid);
-            Console.WriteLine("HandleClientAsync가 전달 받은 client_socket 인스턴스가 유효하지 않음");
-            return;
-        }
-
-        byte[] bytes = new byte[1024];
-        int name_len = await client_socket.GetStream().ReadAsync(bytes, 0, bytes.Length);
-        string user_name = Encoding.UTF8.GetString(bytes, 0, name_len);
-
-        if (name_len > 0 && user_name is not "leave" && user_name.Length <= 10 && user_name.Contains(" ")
-            && redis_connector.write_user_info(user_guid, user_name))
-        {
-            var client_grain = _grainFactory.GetGrain<IChatClientGrain>(user_guid);
-            Console.WriteLine($"Redis에 기록 성공! 어서오세요 {user_name}님");
-            await client_grain.packet_worker();
-        }
-        else
-        {
-            redis_connector.delete_user_info(user_guid);
-            Console.WriteLine("Redis에 기록 실패...");
-
-            client_connector.discoonect_client(user_guid);
-            Console.WriteLine("클라이언트 연결 해제됨");
+            
+            switch(buffer[0]) 
+            {
+                
+            }
         }
     }
 
