fb6e0c5184373838400087c920ddc7fd8a366109
mgcllee <mgcllee@gmail.com>
2025-06-04 15:11:16 +0900

    [OrleansServer][update] Define interface method

--- TrickFarmServer/TrickFarmServerApp/TrickFarmOrleansServer/interfaces/IClientConnector.cs
+++ TrickFarmServer/TrickFarmServerApp/TrickFarmOrleansServer/interfaces/IClientConnector.cs
@@ -2,6 +2,10 @@
 
 public interface IClientConnector
 {
-    
+    Task start_client_accepter();
+    Task check_exist_client();
+    Task discoonect_client(Guid user_guid);
     Task disconnect_all_clients();
+
+    void stop_server_accepter();
 }
--- TrickFarmServer/TrickFarmServerApp/TrickFarmOrleansServer/interfaces/IRedisConnector.cs
+++ TrickFarmServer/TrickFarmServerApp/TrickFarmOrleansServer/interfaces/IRedisConnector.cs
@@ -2,4 +2,6 @@
 {
     bool write_user_info(Guid user_guid, string user_name);
     string? get_user_name(Guid user_guid);
+    bool delete_user_info(Guid user_guid);
+    void disconnect_redis();
 }
